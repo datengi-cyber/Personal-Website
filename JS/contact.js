16708b5bad21614696f9e4a2686ab31266d26609
Tenzi sherpa <np03cs4a220403@heraldcollege.edu.np>
2023-02-11 13:42:05 +0545

    Persona Website using HTML CSS AND JS

---
+++ JS/contact.js
@@ -0,0 +1,17 @@
+alert("Please Enter your name to contact me  ")
+
+console.log(document.getElementById("paragraph").innerHTML);
+console.log(typeof document)
+
+
+console.log("something ");
+document.getElementById("paragraph").style.cssText="color:white background:black";
+document.getElementById("paragraph").style.color="white";
+document.getElementById("paragraph").style.background="black";
+
+var name="Datengi"
+console.log(typeof age);
+console.log( name);
+console.log(typeof phonenumber);
+document.getElementById("message").innerHTML=`hello i am ${name}, I am gald that you have visited my website`;
+console.log(prompt("enter a name:"))
